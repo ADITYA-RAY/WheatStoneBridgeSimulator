cc471364070ea8a88c7ebd43fa65c7372f88443d
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2022-01-01 17:37:23 +0530

    Control panel css updated

--- main.js
+++ main.js
@@ -1,30 +1,108 @@
 // Aditya Ghidora
 // source for the formulae
 // https://en.wikipedia.org/wiki/Wheatstone_bridge
-let r1 = 5,r2 = 5, r3 = 5, r4 = 5, vs = 10;
 
-function galvanometer(r1,r2,r3,r4,vs)
-{
-    // Takes the 4 resistances and a supply voltage
-    // outputs the galvanometer voltage
-    let first = r2/(r1+r2), second = r4/(r3+r4);
-    let vg = (first - second)*vs;
-    return vg;
+var unknown = 20,
+  supply = 20,
+  set = false,
+  running = false,
+  final;
+function galvanometer(r1, r2, r3, r4, vs) {
+  // Takes the 4 resistances and a supply voltage
+  // outputs the galvanometer voltage
+  let first = r2 / (r1 + r2),
+    second = r4 / (r3 + r4);
+  let vg = (first - second) * vs;
+  return vg;
 }
 
-function unkownResistance(r1,r2,r3,vg,vs)
-{
-    // Takes the first 3 resistances, galvanometer voltage and supply voltage
-    // outputs the unknown resistance
-    let numerator = r2*vs - (r1+r2)*vg;
-    let denominator = r1*vs +(r1+r2)*vg;
-    let rx = (numerator*r3)/denominator;
-    return rx;
+function unkownResistance(r1, r2, r3, vg, vs) {
+  // Takes the first 3 resistances, galvanometer voltage and supply voltage
+  // outputs the unknown resistance
+  let numerator = r2 * vs - (r1 + r2) * vg;
+  let denominator = r1 * vs + (r1 + r2) * vg;
+  let rx = (numerator * r3) / denominator;
+  return rx;
 }
-
-// let vg1 = galvanometer(r1,r2,r3,r4,vs);
-
-// console.log(vg1);
-// console.log(unkownResistance(r1,r2,r3,vg1,vs));
 
 // Aditya Ghidora
+
+//aditya ray----------------
+function matchValues(a, b) {
+  if (set && running) {
+    var rs1 = document.getElementById("rs1").value;
+    var rs2 = document.getElementById("rs2").value;
+    var rs3 = document.getElementById("rs3").value;
+    document.getElementById(b).value = document.getElementById(a).value;
+    var vg = galvanometer(rs1, rs2, rs3, unknown, supply);
+    document.getElementById("vg").value = vg.toPrecision(8);
+    final = unkownResistance(rs1, rs2, rs3, vg, supply);
+  }
+}
+function swap(a, b) {
+  document.getElementById(b).value = document.getElementById(a).value;
+}
+function setResistance() {
+  if (!running) {
+    set = true;
+    unknown = document.getElementById("rsx").value;
+    supply = document.getElementById("vs").value;
+  } else {
+    alert("Switch off the circuit first.");
+  }
+}
+function switchCircuit() {
+  if (!running) {
+    running = true;
+    document.querySelector(".switch").innerHTML =
+      '<img src="./images/switchonn.png" alt="" class="switchonn" onclick="switchCircuit()">';
+  } else {
+    running = false;
+    document.querySelector(".switch").innerHTML =
+      '<img src="./images/switchoff.png" alt="" class="switchonn" onclick="switchCircuit()">';
+  }
+}
+function simulate() {
+  if (running) {
+    document.getElementById("sim").value = final;
+  } else {
+    alert(
+      "Please Switch on the supply to verify the milivoltmeter reading first."
+    );
+  }
+}
+//aditya ray----------
+
+var f1 = document.getElementById("AlimCard1");
+var f2 = document.getElementById("AlimCard2");
+
+
+
+function fun1(){
+    
+    document.getElementById("AlimCard1").style.display = "block"
+    document.getElementById("AlimCard2").style.display = 'none';
+
+    document.getElementById("f1").style.boxShadow = 'inset 0.15em 0.15em 0.15em var(--shadow-white), inset -0.15em -0.15em 0.15em var(--shadow-gray), 0.3rem 0.3rem 0.6rem var(--shadow-gray), -0.3rem 0rem 0.6rem var(--shadow-white)'
+    document.getElementById("f1").style.color = '#6d5dfc'
+
+    document.getElementById("f2").style.boxShadow = 'none';
+    document.getElementById("f2").style.color = 'black';
+
+
+    
+}
+function fun2(){
+
+  onmouseover="bigImg(this)"
+
+
+
+  document.getElementById("AlimCard1").style.display = 'none';
+    document.getElementById("AlimCard2").style.display = 'block';
+    document.getElementById("f2").style.boxShadow = 'inset 0.15em 0.15em 0.15em var(--shadow-white), inset -0.15em -0.15em 0.15em var(--shadow-gray), 0.3rem 0.3rem 0.6rem var(--shadow-gray), -0.3rem 0rem 0.6rem var(--shadow-white)'
+    document.getElementById("f2").style.color = '#6d5dfc'
+
+    document.getElementById("f1").style.boxShadow = 'none';
+    document.getElementById("f1").style.color = 'black';
+}
